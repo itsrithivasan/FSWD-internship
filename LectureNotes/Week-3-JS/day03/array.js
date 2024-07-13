611f12f544ad0bfaee7be9301919416d823585d5
itsrithivasan <itsrithivasan@gmail.com>
2024-07-13 11:41:50 +0530

    Week 3 day 3 lecture notes updated

---
+++ LectureNotes/Week-3-JS/day03/array.js
@@ -0,0 +1,22 @@
+let arr = [1,2,3,4,5,"peter","Hulk"];
+// let data = new Array("a","b","c");
+// console.log(arr);
+// console.log(data);
+
+// console.log(arr.length);
+
+let avengers = ["spiderman","batmen","superman","thor", "ironman","hulk"];
+// avengers.push("Rithi");
+// avengers.pop;
+// console.log(avengers);
+
+
+// from the start shift and unshift
+// avengers.unshift("Rithi");
+// avengers.shift();
+// console.log(avengers);
+
+let fruits = ["apple","banana","kiwi","mangoe","cherry","plum"];
+console.log(fruits);
+fruits.splice(3,0,"watermelon");
+console.log(fruits);
